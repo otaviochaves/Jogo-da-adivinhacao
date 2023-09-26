c72dc72861e7ba0a46ae3ca68f0d7b315ea9351f
Otavio Chaves <otaviochaves858@gmail.com>
2023-09-26 00:16:42 -0300

    Foi adicionado um evento de click para clicar na tela e tirar o foco do input.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
@@ -7,7 +6,16 @@ const h2Text = document.querySelector('.screen2 h2')
 let randomNumber = Math.round(Math.random() * 10) // foi trocado de const para let, para colocar lá embaixo
 let xAttempts = 1;
 
-// Eventos
+document.addEventListener('click', (event) =>
+{
+    if (event.target !== inputVelocidadeDeCorte && event.target !== inputDiametroDaPeca)
+    {
+        inputVelocidadeDeCorte.blur();
+        inputDiametroDaPeca.blur();
+    }
+});
+
+
 btnTry.addEventListener('click', handleTryClick)
 btnReset.addEventListener('click', handleResetClick)
 document.addEventListener('keydown', function (e)
@@ -18,11 +26,10 @@ document.addEventListener('keydown', function (e)
     }
 })
 
-// Funções 
-// callback (chamar de volta, que em algum momento será utilizada novamente)
+
 function handleTryClick (event)
 {
-    event.preventDefault() // não faça o padrão do evento (é enviar o formulário, portanto, recarrega a página)
+    event.preventDefault()
 
     let inputValue = document.querySelector('input').value
 
@@ -46,7 +53,7 @@ function handleResetClick ()
 {
     toggleScreen()
     xAttempts = 1
-    randomNumber = Math.round(Math.random() * 10) //e assim o número ser gerado sempre que fizer o reset
+    randomNumber = Math.round(Math.random() * 10)
 }
 
 function toggleScreen ()
